2d5af8dab7a67cbf1f5f60fbc3707a5e2a67c401
yixinsun <121929989@qq.com>
2022-12-22 23:28:00 -0800

    feat: add basic REST API

---
+++ currency-conversion-service/src/main/java/com/yixin/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,16 @@
+package com.yixin.microservices.currencyconversionservice.controller;
+
+import com.yixin.microservices.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+}
