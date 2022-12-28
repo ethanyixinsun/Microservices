21dd2ad356e458cbf5544422156c029046a96c9a
yixinsun <121929989@qq.com>
2022-12-27 21:52:01 -0800

    feat: enable CircuitBreaker, RateLimiter, and Bulkhead

--- currency-exchange-service/src/main/java/com/yixin/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/yixin/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.yixin.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +16,16 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name="sample-api")
+    @Bulkhead(name="sample-api")
     public String sampleApi() {
         logger.info("Sample api call received");
-//        return "Sample API";
-        ResponseEntity<String> forEntity =
-            new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-        return forEntity.getBody();     // this would fail!
+        return "Sample API";        // this would success!
+//        ResponseEntity<String> forEntity =
+//            new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//        return forEntity.getBody();     // this would fail!
     }
 
     public String hardcodedResponse(Exception exception) {
